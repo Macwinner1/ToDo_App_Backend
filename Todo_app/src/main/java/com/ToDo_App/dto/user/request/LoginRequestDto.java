40b03863d9bfb2594ae51174e352a999c6e44399
Macwinner1 <ifechukwuw@gmail.com>
2025-09-14 16:21:28 +0100

    working on LoginUser

---
+++ Todo_app/src/main/java/com/ToDo_App/dto/user/request/LoginRequestDto.java
@@ -0,0 +1,13 @@
+package com.ToDo_App.dto.user.request;
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Data;
+
+@Data
+public class LoginRequestDto {
+    @NotEmpty(message = "Username cannot be empty")
+    private String username;
+
+    @NotEmpty(message = "Password cannot be empty")
+    private String password;
+}
