b6e7a39f2f74824e6bbfbd3b1181a5814d95dc35
Macwinner1 <ifechukwuw@gmail.com>
2025-09-15 17:57:12 +0100

    token response dto

---
+++ Todo_app/src/main/java/com/ToDo_App/dto/token/response/TokenResponseDto.java
@@ -0,0 +1,21 @@
+package com.ToDo_App.dto.token.response;
+
+import com.ToDo_App.dto.BaseResponseDto;
+import com.ToDo_App.dto.token.TokenDto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.springframework.http.HttpStatusCode;
+
+@EqualsAndHashCode(callSuper=true)
+@Data
+@AllArgsConstructor
+public class TokenResponseDto extends BaseResponseDto {
+    private String accessToken;
+    private String refreshToken;
+    public TokenResponseDto(HttpStatusCode statusCode, String statusMessage, TokenDto tokenDto) {
+        super(statusCode, statusMessage);
+        this.accessToken = tokenDto.getAccessToken();
+        this.refreshToken = tokenDto.getRefreshToken();
+    }
+}
