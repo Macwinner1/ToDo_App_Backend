737e5d3cf0a3bd1508f990ec4f524ab2e920d775
Macwinner1 <ifechukwuw@gmail.com>
2025-09-15 18:01:09 +0100

    user dto

---
+++ Todo_app/src/main/java/com/ToDo_App/dto/user/response/UserDto.java
@@ -0,0 +1,28 @@
+package com.ToDo_App.dto.user.response;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+public class UserDto {
+
+    private UUID userId;
+
+    @NotEmpty(message = "Username cannot be empty")
+    @Size(min = 4, message = "The length of the username should be atleast 4")
+    private String username;
+
+    @NotEmpty(message = "First Name cannot be empty")
+    @Size(min = 2, message = "The lenght of the first name should be atleast 2")
+    private String firstName;
+
+    private String lastName;
+
+    @NotEmpty(message = "Email cannot be empty")
+    @Email(message = "Invalid email format")
+    private String email;
+}
