738bf59e41ada0b3cc57e5650e7acd594e28a2e4
Macwinner1 <ifechukwuw@gmail.com>
2025-09-15 17:54:56 +0100

    exception handling class

---
+++ Todo_app/src/main/java/com/ToDo_App/expections/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package com.ToDo_App.expections;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String resourceName, String fieldName, String fieldValue) {
+        super(String.format("Resource %s not found with name %s and value %s", resourceName, fieldName, fieldValue));
+    }
+}
---
+++ Todo_app/src/main/java/com/ToDo_App/expections/UserNotAuthenticatedException.java
@@ -0,0 +1,11 @@
+package com.ToDo_App.expections;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.UNAUTHORIZED)
+public class UserNotAuthenticatedException extends RuntimeException {
+    public UserNotAuthenticatedException(String message) {
+        super(message);
+    }
+}
