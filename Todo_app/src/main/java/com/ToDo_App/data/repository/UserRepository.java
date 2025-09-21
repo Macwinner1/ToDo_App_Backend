693cb7a9daaab70a714605a6ecbf428cffed0a4b
Macwinner1 <ifechukwuw@gmail.com>
2025-09-22 00:16:38 +0100

    update on my UserRepository to reflect what's on my implementation class

--- Todo_app/src/main/java/com/ToDo_App/data/repository/UserRepository.java
+++ Todo_app/src/main/java/com/ToDo_App/data/repository/UserRepository.java
@@ -11,11 +11,10 @@ import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
-    @Query("SELECT u FROM User u WHERE u.userName = :username")
+    @Query("SELECT u FROM User u WHERE u.userName = :userName")
     Optional<User> findByUsername(@Param("userName") String userName);
 
-    @Query("SELECT u FROM User u WHERE u.userName = :username OR u.email = :email")
+    @Query("SELECT u FROM User u WHERE u.userName = :userName OR u.email = :email")
     Optional<User> findByUsernameOrEmail(@Param("userName") String userName, @Param("email") String email);
 
-    String userName(String userName);
 }
