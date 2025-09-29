ab423e93bf6f36b2b6fb185f367858ec7caa0f38
Macwinner1 <ifechukwuw@gmail.com>
2025-09-29 05:24:31 +0100

    updated userService

--- Todo_app/src/main/java/com/ToDo_App/services/impl/IUserService.java
+++ Todo_app/src/main/java/com/ToDo_App/services/impl/IUserService.java
@@ -3,9 +3,7 @@ package com.ToDo_App.services.impl;
 
 import com.ToDo_App.data.models.User;
 import com.ToDo_App.data.repository.UserRepository;
-import com.ToDo_App.expections.UserNotAuthenticatedException;
 import com.ToDo_App.services.UserService;
-import com.ToDo_App.utils.AuthenticationContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.stereotype.Service;
@@ -18,10 +16,6 @@ public class IUserService implements UserService {
     @Autowired
     private UserRepository userRepository;
 
-    public String hashPassword(String plainPassword) {
-        return BCrypt.hashpw(plainPassword, BCrypt.gensalt(12));
-    }
-
     @Override
     public Optional<User> authenticate(String username, String password) {
         Optional<User> userOpt = userRepository.findByUsername(username);
@@ -30,16 +24,4 @@ public class IUserService implements UserService {
         }
         return Optional.empty();
     }
-
-    @Override
-    public Optional<User> getAuthenticatedUser() {
-        User user = AuthenticationContext.getCurrentUser();
-        if (user == null) {
-            throw new UserNotAuthenticatedException("User not authenticated");
-        }
-        return Optional.of(user);
-    }
-
-
-
 }
