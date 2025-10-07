07ab08e3b536b9d65ffe3baa394a6d2b3b79099a
Macwinner1 <ifechukwuw@gmail.com>
2025-10-07 17:01:04 +0100

    updated login dto

--- Todo_app/src/main/java/com/ToDo_App/dto/user/request/LoginRequestDto.java
+++ Todo_app/src/main/java/com/ToDo_App/dto/user/request/LoginRequestDto.java
@@ -1,13 +1,16 @@
 package com.ToDo_App.dto.user.request;
 
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class LoginRequestDto {
     @NotEmpty(message = "Username cannot be empty")
+    @Size(min = 4, message = "Username must be at least 4 characters")
     private String username;
 
     @NotEmpty(message = "Password cannot be empty")
+    @Size(min = 8, message = "Password must be at least 8 characters")
     private String password;
 }
