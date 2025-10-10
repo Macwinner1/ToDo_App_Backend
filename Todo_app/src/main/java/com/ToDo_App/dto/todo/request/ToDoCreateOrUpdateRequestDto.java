1ff412a5e77734f8b179adf0740acdd9060c6309
Macwinner1 <ifechukwuw@gmail.com>
2025-10-10 04:52:29 +0100

    ToDoCreateOrUpdateRequestDto updated

--- Todo_app/src/main/java/com/ToDo_App/dto/todo/request/ToDoCreateOrUpdateRequestDto.java
+++ Todo_app/src/main/java/com/ToDo_App/dto/todo/request/ToDoCreateOrUpdateRequestDto.java
@@ -1,26 +1,24 @@
 package com.ToDo_App.dto.todo.request;
 
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
-import lombok.*;
+import lombok.Data;
 
 @Data
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
 public class ToDoCreateOrUpdateRequestDto {
-    @Size(min = 2, max = 255, message = "The length of the title should be between 2 and 255")
     @NotEmpty(message = "Title cannot be empty")
+    @Size(min = 2, max = 255, message = "The length of the title should be between 2 and 255")
     private String title;
 
-    @Size(max = 2048, message = "The length  of the description should be less than 2048")
+    @Size(max = 2048, message = "The length of the description should be less than 2048")
     private String description;
 
     private boolean completed;
 
+    @NotEmpty(message = "Priority cannot be empty")
+    @Pattern(regexp = "LOW|MEDIUM|HIGH", message = "Priority must be LOW, MEDIUM, or HIGH")
     private String priority;
 
     private String dueDate;
 }
-
