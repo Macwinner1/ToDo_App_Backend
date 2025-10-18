4b7fc3e17ba392173ee35505d7eec635cdfb61a7
Macwinner1 <ifechukwuw@gmail.com>
2025-10-18 22:12:59 +0100

    exception class added

---
+++ Todo_app/src/main/java/com/ToDo_App/exceptions/InvalidCredentialsException.java
@@ -0,0 +1,9 @@
+package com.ToDo_App.exceptions;
+
+public class InvalidCredentialsException extends RuntimeException {
+    public InvalidCredentialsException(String message) {
+        super(message);
+    }
+
+
+}
